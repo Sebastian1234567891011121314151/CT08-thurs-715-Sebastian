c6638b2a4d292ec1867ff05b882476874cef37c5
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-07-23 12:49:52 +0000

    Jul 23, 2026, 12:49 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -97,7 +97,7 @@ function draw() {
     playerJump = 0;
    }
 
-   if(player.collides(spikes)|| player.x<leftEdge&&player.y>leftEgeHeight){
+   if(player.collides(spikes) ){
     resetGame()
    }
 
