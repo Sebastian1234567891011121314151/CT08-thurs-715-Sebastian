5b3bd930d2905dfbf39131c6dc12204c4f1ecaa0
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-07-23 12:32:21 +0000

    Jul 23, 2026, 12:32 PM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -89,7 +89,7 @@ function draw() {
    }
 
    if((kb.presses("space")||mouse.presses("left"))&&playerJump==0){
-    player.vel.y=-10;
+    player.vel.y=-8;
     player.rotateTo(player.rotation + 359.9,15);
     playerJump++;
    }
