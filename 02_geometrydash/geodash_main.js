671602189b2b07f5d18b9538d0282cc9f06be500
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-07-23 11:30:42 +0000

    Jul 23, 2026, 11:30 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -49,7 +49,7 @@ function setup() {
   ground.stroke="white";
   ground.collider="static";
 
-  new Tiles(tileMap1,0,0,TILE_SIZE,T);
+  new Tiles(tileMap1,0,0,TILE_SIZE,TILE_SIZE);
 }
 
 function draw() {
