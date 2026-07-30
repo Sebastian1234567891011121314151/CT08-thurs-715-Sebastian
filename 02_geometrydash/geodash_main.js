3c7a8cca16759e87f22a6c1f35a88157c0d43bb5
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-07-30 11:40:04 +0000

    Jul 30, 2026, 11:40 AM

--- 02_geometrydash/geodash_main.js
+++ 02_geometrydash/geodash_main.js
@@ -39,7 +39,8 @@ cube = loadImage("assets/cube.png");
 bg= loadImage("assets/geobg.png");
 tileMap1=loadStrings("stages/tiles1.txt");
 spike=loadImage("assets/spike.png");
-startGameImg = (loadImage("assets/startgame.png"))
+startGameImg = loadImage("assets/startgame.png");
+
 }
 
 function setup() {
