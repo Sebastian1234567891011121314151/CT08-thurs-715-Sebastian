b623ce1877cf10e02260647165b7a533313304ab
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-08-13 12:24:05 +0000

    Aug 13, 2026, 12:24 PM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -10,6 +10,17 @@ function preload() {}
 
 function setup() {}
     createCanvas(videoW, vidoeH);
+    let constraints = {
+        video: {
+            mandatory: {
+                minWidth: videoW,
+                minHeight: videoH
+            },
+            optional: [{ minFrameRate: 60 }]
+        },
+        audio: false,
+        flipped: true
+    };
 function draw() {}
 
 //=========================================
