cf8117d68c00571fd039ae1793f8159b392fbc63
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-09-10 12:01:47 +0000

    Sep 10, 2026, 12:01 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -51,6 +51,16 @@ function setup() {
     video.hide();
 
     handPose.detectStart(video, gotHands);
+
+    balloon=new Sprite();
+balloon.x=width/2;
+balloon.y=height*0.1;
+balloon.diameter=80;
+balloon.collider="dynamic";
+balloon.color="yellow";
+balloon.bounciness=0.8;
+balloon.mass=5;
+balloon.drag=0;
 }
 
 
