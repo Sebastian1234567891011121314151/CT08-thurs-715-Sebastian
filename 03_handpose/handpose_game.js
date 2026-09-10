ad1da617b551b990b8afbf315ea921637394a3c1
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-09-10 12:44:31 +0000

    Sep 10, 2026, 12:44 PM

--- 03_handpose/handpose_game.js
+++ 03_handpose/handpose_game.js
@@ -62,6 +62,34 @@ function setup() {
     balloon.bounciness=0.8;
     balloon.mass=5;
     balloon.drag=0;
+
+    leftWall=new Sprite();
+    leftWall.x=0;
+    leftWall.y=height/2;
+    leftWall.width=10;
+    leftWall.height=height;
+    leftWall.collider="static";
+
+    rightWall=new Sprite();
+    rightWall.x=width;
+    rightWall.y=height/2;
+    rightWall.width=10;
+    rightWall.height=height;
+    rightWall.collider="static";
+
+    topWall=new Sprite();
+    topWall.x=width/2;
+    topWall.y=0;
+    topWall.width=width;
+    topWall.height=10;
+    topWall.collider="static";
+
+    bottomWall=new Sprite();
+    bottomWall.x=width/2;
+    bottomWall.y=height;
+    bottomWall.width=width;
+    bottomWall.height=10;
+    bottomWall.collider="static";
 }
 
 
@@ -71,14 +99,12 @@ function gotHands(results) {
 function draw() {
     image(video, 0, 0, videoW, videoH);
 
-    for(let i=0; i<hands.length; i++){
-        let hand = hands[i];
-        // loop through all the 21 keypoints
-        for (let j = 0; j < hand.keypoints.length; j++) {
-            let keypoint = hand.keypoints[8];
-
-            // for every keypoint, draw a circle.
-            circle(keypoint.x, keypoint.y, 10);
-        }
+    for(let i=0; i<hands.length; i++){ 
+        let hand = hands[i]; 
+        let keypoint = hand.keypoints[8]; 
+        fingerTip.x=keypoint.x; 
+        fingerTip.y=keypoint.y;
     }
+
+
 }
