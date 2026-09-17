44bcead0a3ea9294048e7353948dd71244ae6aa5
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-09-17 11:57:29 +0000

    Sep 17, 2026, 11:57 AM

--- 03_handpose/handpose_basic.js
+++ 03_handpose/handpose_basic.js
@@ -114,7 +114,7 @@ function draw() {
         bounceSound.play();
     }
 
-    // BALL BOUNCES WHEN FINGER HITS IT
+    // BALL BOUNCES WHEN FINGER H
     if (ball.collides(fingerTip)) {
         ball.vel.y=-8;
     }
